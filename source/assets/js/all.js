02b445fbc97d731b641958c576eaee661b814408
ononao <naonao230410@gmail.com>
2015-10-27 15:29:28 +0900

    作業中

---
+++ source/assets/js/all.js
@@ -0,0 +1,110 @@
+/* DOMの読み込み完了後に処理 */
+if(window.addEventListener) {
+	window.addEventListener( "load" , shareButtonReadSyncer, false );
+}else{
+	window.attachEvent( "onload", shareButtonReadSyncer );
+}
+
+/* シェアボタンを読み込む関数 */
+function shareButtonReadSyncer(){
+
+//遅延ロードする場合は次の行と、終わりの方にある行のコメント(//)を外す
+//setTimeout(function(){
+
+//Twitter
+window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));
+
+//Facebook
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.0";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+//Google+
+var scriptTag = document.createElement("script");
+scriptTag.type = "text/javascript"
+scriptTag.src = "https://apis.google.com/js/platform.js";
+scriptTag.async = true;
+document.getElementsByTagName("head")[0].appendChild(scriptTag);
+
+//はてなブックマーク
+var scriptTag = document.createElement("script");
+scriptTag.type = "text/javascript"
+scriptTag.src = "https://b.st-hatena.com/js/bookmark_button.js";
+scriptTag.async = true;
+document.getElementsByTagName("head")[0].appendChild(scriptTag);
+
+//pocket
+(!function(d,i){if(!d.getElementById(i)){var j=d.createElement("script");j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";var w=d.getElementById(i);d.body.appendChild(j);}}(document,"pocket-btn-js"));
+
+//},5000);	//ページを開いて5秒後(5,000ミリ秒後)にシェアボタンを読み込む
+
+}
+
+
+
+// グローバル変数
+var syncerTimeout = null ;
+
+// 一連の処理
+$( function()
+{
+	// スクロールイベントの設定
+	$( window ).scroll( function()
+	{
+		// 1秒ごとに処理
+		if( syncerTimeout == null )
+		{
+			// セットタイムアウトを設定
+			syncerTimeout = setTimeout( function(){
+
+				// 対象のエレメント
+				var element = $( '#pageTop' ) ;
+
+				// 現在、表示されているか？
+				var visible = element.is( ':visible' ) ;
+
+				// 最上部から現在位置までの距離を取得して、変数[now]に格納
+				var now = $( window ).scrollTop() ;
+
+				// 最下部から現在位置までの距離を計算して、変数[under]に格納
+				var under = $( 'body' ).height() - ( now + $(window).height() ) ;
+
+				// 最上部から現在位置までの距離(now)が1500以上かつ
+				// 最下部から現在位置までの距離(under)が200px以上かつ…
+				if( now > 10000 )
+				{
+					// 非表示状態だったら
+					if( !visible )
+					{
+						// [#page-top]をゆっくりフェードインする
+						element.fadeIn( 'slow' ) ;
+					}
+				}
+
+				// 1500px以下かつ
+				// 表示状態だったら
+				else if( visible )
+				{
+					// [#page-top]をゆっくりフェードアウトする
+					element.fadeOut( 'fast' ) ;
+				}
+
+				// フラグを削除
+				syncerTimeout = null ;
+			} , 1000 ) ;
+		}
+	} ) ;
+
+	// クリックイベントを設定する
+	$( '#pageTop' ).click(
+		function()
+		{
+			// スムーズにスクロールする
+			$( 'html,body' ).animate( {scrollTop:0} , 4000 ) ;
+		}
+	) ;
+} ) ;
